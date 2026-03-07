95741dbcc0f1706ea6b7c41aac1e9780dbc7aa67
재섭 <wotjw734843@gmail.com>
2026-03-07 18:18:11 +0900

    login & products api

---
+++ tests/api/test_api_login.py
@@ -0,0 +1,36 @@
+import requests
+import allure
+
+BASE_URL = "https://automationexercise.com/api"
+
+
+@allure.epic("사용자 인증")
+@allure.feature("로그인 API")
+
+class TestLoginAPI:
+
+    allure.story("정상 로그인")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description("유효한 계정으로 로그인 API 호출 시 200 응답과 토큰 확인")
+    
+    def test_login_sucess(self):
+        with allure.step("로그인 API 호출"):
+            response = requests.post(
+                f"{BASE_URL}/login",
+                data={"email":"jasubtest@example.com", "password":"test1234"}
+            )
+        with allure.step("응답 상태 코드 확인"):
+            assert response.status_code == 200, "응답 상태 코드는 200이어야 합니다"
+    
+    def test_login_failure(self):
+        with allure.step("잘못된 로그인 API 호출"):
+            response = requests.post(
+                f"{BASE_URL}/login",
+                data={"email":"test@example.com","password":"asdfd123x"}
+            )
+        
+        with allure.step("응답 상태 코드 확인"):
+            assert response.status_code == 200
+        with allure.step("로그인 실패 메시지 확인"):
+            data = response.json()
+            assert data["responseCode"] == 403
---
+++ tests/api/test_api_products.py
@@ -0,0 +1,42 @@
+import requests
+import allure
+
+BASE_URL = "https://automationexercise.com/api"
+
+@allure.epic("상품")
+@allure.feature("상품 API")
+class TestProductsAPI:
+
+    @allure.story("상품 목록 조회")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description("상품 목록 API 호출 시 200 응답과 상품 데이터 확인")
+    def test_get_products(self):
+        with allure.step("상품 목록 API 호출"):
+            response = requests.get(f"{BASE_URL}/productsList")
+
+        with allure.step("응답 상태 코드 확인"):
+            assert response.status_code == 200, "응답 상태 코드는 200이어야 합니다"
+
+        with allure.step("상품 데이터 확인"):
+            data = response.json()
+            assert "products" in data, "응답에 'products' 키가 있어야 합니다"
+            assert isinstance(data["products"], list), "'products'는 리스트여야 합니다"
+            assert len(data["products"]) > 0, "상품이 최소 1개 이상 있어야 합니다"
+
+    @allure.story("상품 검색 api")
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.description("상품 검색 API 호출 시 200 응답과 검색 결과 확인")
+    def test_search_products(self):
+        with allure.step("/searchProduct API 호출 (키워드: 'dress')"):
+            response = requests.post(
+                f"{BASE_URL}/searchProduct",
+                data={"search_product": "dress"}
+            )
+
+        with allure.step("상태 코드 200 확인"):
+            assert response.status_code == 200, "응답 상태 코드는 200이어야 합니다."
+
+        with allure.step("검색 결과 1개 이상 확인"):
+            data = response.json()
+            assert "products" in data
+            assert len(data["products"]) > 0, "검색 결과가 최소 1개 이상이여야 합니다."
